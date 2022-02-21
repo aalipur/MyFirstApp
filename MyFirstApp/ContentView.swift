68bba85a7185cd6d0c4f8cf4e7fc50a3f388dca1
aalipur <aalipur@icloud.com>
2022-02-21 10:43:19 +0300

    Lesson 17 Modifiers

--- MyFirstApp/ContentView.swift
+++ MyFirstApp/ContentView.swift
@@ -7,64 +7,52 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CustomText: View {
     
-    @State private var countries = ["UK", "USA", "Bangladesh", "Germany", "Argentina", "Brazil", "Canada", "Greece", "Russia", "Sweden"].shuffled()
-    @State private var correctAnswer = Int.random(in: 0...2)
-    @State private var score = 0
-    @State private var showingScore = false
-    @State private var scoreTitle = ""
-    
+    var name: String
     var body: some View {
-        ZStack {
-            LinearGradient(gradient: Gradient(colors: [.black, .white]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
-            VStack(spacing: 30) {
-                VStack {
-                    Text("Выбери флаг: ")
-                        .foregroundColor(.white)
-                        .font(.headline)
-                    Text(countries[correctAnswer])
-                        .foregroundColor(.white)
-                        .font(.largeTitle)
-                        .fontWeight(.black)
-                }
-                ForEach(0..<3) { number in
-                    Button(action: {
-                        self.flagTapped(number)
-                        self.showingScore = true
-                    }) {
-                        Image(self.countries[number])
-                            .renderingMode(.original)
-                            .frame(width: 250, height: 125)
-                            .clipShape(Capsule())
-                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
-                            .shadow(color: .black, radius: 2)
-                    }
-                }
-                Text("Общий счет: \(score)")
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-                Spacer()
-            }
-        } .alert(isPresented: $showingScore) {
-            Alert(title: Text(scoreTitle), message: Text("Общий счет: \(score)"), dismissButton: .default(Text("продолжить")))
-        }
-    }
-    func askQuestion() {
-        countries.shuffle()
-        correctAnswer = Int.random(in: 0...2)
-    }
-    func flagTapped(_ number: Int) {
-        if number == correctAnswer {
-            scoreTitle = "Правильный ответ"
-            score += 1
-        } else {
-            scoreTitle = "Неправильно! Это флаг \(countries[number])"
-            score -= 1
-        }
+        Text(name)
+            .font(.largeTitle)
+            .padding()
+            .foregroundColor(.red)
+            .background(Color.green)
     }
 }
 
+struct CustomModifier: ViewModifier {
+    
+    func body(content : Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(Color.black)
+    }
+}
+
+struct ContentView: View {
+    
+    //@State private var useGreenText = false
+    
+    var body: some View {
+        /*Button("Hello, World!") { // условный модификатор
+            self.useGreenText.toggle()
+        } .foregroundColor(useGreenText ? .green: .blue) */
+        /*VStack(spacing: 30) {
+            CustomText(name: "First")
+            CustomText(name: "Second")
+        } */
+        Text("Hello, Swift!")
+            .CustomM()
+    }
+}
+
+extension View {
+    
+    func CustomM() -> some View {
+        self.modifier(CustomModifier)
+    }
+}
 
 
 
